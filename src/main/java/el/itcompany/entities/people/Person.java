42cbe143b041a39e38cf144e3a6d41a02898846e
ericluec <eric.luecking@gmail.com>
2025-06-05 18:11:48 +0200

    many attempts to silence errors

--- src/main/java/el/itcompany/entities/people/Person.java
+++ src/main/java/el/itcompany/entities/people/Person.java
@@ -1,9 +1,61 @@
 package el.itcompany.entities.people;
 
-public interface Person {
+import el.itcompany.entities.building.DefaultBuilding;
+import el.itcompany.entities.inventory.Item;
+import el.itcompany.entities.position.Position;
+import el.itcompany.entities.sectors.IT_Sector;
+import jakarta.persistence.*;
+import lombok.*;
 
-    boolean isOpenForWork();
-    String reportPerson(String message);
-    Manager getManager();
-    void listItemsInPosession();
+import java.util.ArrayList;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DefaultPerson implements Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int age;
+    String name;
+    @Transient
+    Position position;
+    @Transient
+    Manager manager;
+    ArrayList<Item> itemsInPosession = new ArrayList<>();
+    @Transient
+    IT_Sector sector;
+    @Transient
+    DefaultBuilding workPlace;
+    ArrayList<String> complaints = new ArrayList<>();
+
+    @Override
+    public String reportPerson(String message) {
+        this.complaints.add(message);
+        return message;
+    }
+
+    @Override
+    public boolean isOpenForWork() {
+        return false;
+    }
+
+    @Override
+    public Manager getManager() {
+        return this.manager;
+    }
+
+    @Override
+    public void listItemsInPosession() {
+        if (!this.itemsInPosession.isEmpty()) {
+            System.out.println("There are no items in posession");
+        }
+        for (Item i : this.itemsInPosession) {
+            System.out.println(i);
+        }
+    }
+
 }
--- src/main/java/el/itcompany/entities/sectors/DefaultSector.java
+++ src/main/java/el/itcompany/entities/sectors/DefaultSector.java
@@ -1,5 +1,23 @@
 package el.itcompany.entities.sectors;
 
-public interface DefaultSector {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class IT_Sector implements  DefaultSector {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    String name;
+
+    public boolean confidential() {
+        return true;
+    }
 }
