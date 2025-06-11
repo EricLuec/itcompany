2bcd303bb0aa5011533e718e0e5e4ffae71574c3
ericluec <eric.luecking@gmail.com>
2025-06-11 06:36:46 +0200

    sector controller added

---
+++ src/main/java/el/itcompany/controllers/SectorController.java
@@ -0,0 +1,51 @@
+package el.itcompany.controllers;
+
+import el.itcompany.entities.Sector;
+import el.itcompany.services.SectorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+public class SectorController {
+
+    private final SectorService sectorService;
+
+    public SectorController(SectorService projectService) {
+        this.sectorService = projectService;
+    }
+
+    @GetMapping
+    public List<Sector> getAllSectors() {
+        return sectorService.getAllSectors();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Sector> getSectorById(@PathVariable Long id) {
+        return sectorService.getSectorById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Sector createSector(@RequestBody Sector sector) {
+        return sectorService.createSector(sector);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Sector> updateSector(@PathVariable Long id, @RequestBody Sector sector) {
+        try {
+            return ResponseEntity.ok(sectorService.updateSector(id, sector));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        sectorService.deleteSector(id);
+        return ResponseEntity.noContent().build();
+    }
+}
